1014ae3d3f8e1af749d0458962ff267a65784654
Kaiser <minh.hackefr89@gmail.com>
2024-09-01 08:17:35 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+//ham hien thi hop len tren cho nguoi dung doc : alert
+//ham hien thi hop len tren cho nguoi dung nhap du lieu : prompt
+//du lieu duoc nhap tu prompt la string
+//trong 1 phep toan cong neu co 1 toan hang la string thi se ep ve kieu string
+//neu phep toan -*/% thif string bi ep sang Number
+//=== so sanh quan tam den kieu du lieu (!==)(giu nguyen kieu du lieu kh chuyen doi)
+//== so sanh kh quan tam den kieu du lieu (!=)
+//dung toFixed() thi se tra ve kieu string
+//ham random tra ve 1 so tu 0->1
